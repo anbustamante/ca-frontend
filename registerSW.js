d527c4269f301eccddc9b24738fee74bc1baf6bc
Andy <andy00b@gmail.com>
2024-10-11 20:35:32 -0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ca-frontend/sw.js', { scope: '/ca-frontend/' })})}
